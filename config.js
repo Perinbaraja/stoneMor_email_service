eb72f609c10b6974887fbe63780d26309e9c4643
ashokchangecx <perinbaraja23@gmail.com>
2023-03-17 11:55:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   MAIL_SERVICE: "gmail",
   SENDER_MAIL: "akris@stonemor.com",
-  MAIL_PASSWORD: "oyzugzzvyhyryfai",
+  MAIL_PASSWORD: "xnbbeaxeejnsurhz",
   RESET_PASSWORD: "dev123",
 
   DEFAULT_RES_ERROR: "Something wrong occured",
